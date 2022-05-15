b1ad5ffc853d2b78becb07e8906f3e36a7fef102
AbonyiXavier <francisabonyi@gmail.com>
2022-05-15 15:33:56 +0100

    minor clean up

--- src/controller/urlShorten.controller.js
+++ src/controller/urlShorten.controller.js
@@ -7,15 +7,6 @@ import generateShortCode from "../uniqueShortCode/shortCode";
 export const createShortUrl = async (request, response) => {
   let { url, shortCode } = request.body;
 
-  const shortCodeExist = await UrlShorten.findOne({ shortCode }).lean();
-
-  if (shortCodeExist) {
-    return response.status(409).json({
-      status: false,
-      error: "Short code already in use",
-    });
-  }
-
   if (!url) {
     return response.status(400).json({
       status: false,
@@ -39,44 +30,41 @@ export const createShortUrl = async (request, response) => {
         error: "Short code must contain at least 4 characters",
       });
     }
+  } else {
+    shortCode = generateShortCode();
   }
 
-  shortCode = generateShortCode();
+  try {
+    const shortCodeExist = await UrlShorten.findOne({ shortCode }).lean();
 
-  if (validUrl.isUri(url)) {
-    try {
-      let urlData = await UrlShorten.findOne({ url }).lean();
-
-      if (urlData) {
-        return response.status(200).json({
-          status: true,
-          message: "Display saved info",
-          data: urlData,
-        });
-      }
-
-      const itemToSave = new UrlShorten({
-        url,
-        shortCode: shortCode,
-      });
-
-      const savedItem = await itemToSave.save();
-
-      return response.status(201).json({
-        status: true,
-        message: "Created successfully",
-        data: {
-          shortCode: savedItem.shortCode,
-          url: savedItem.url,
-        },
-      });
-    } catch (error) {
-      logger.error(error.message);
-      return response.status(500).json({
-        status: true,
-        message: "Operation failed",
+    if (shortCodeExist) {
+      return response.status(409).json({
+        status: false,
+        error: "Short code already in use",
       });
     }
+
+    const itemToSave = new UrlShorten({
+      url,
+      shortCode,
+    });
+
+    await itemToSave.save();
+
+    return response.status(201).json({
+      status: true,
+      message: "Created successfully",
+      data: {
+        shortCode,
+        url,
+      },
+    });
+  } catch (error) {
+    logger.error(error.message);
+    return response.status(500).json({
+      status: true,
+      message: "Operation failed",
+    });
   }
 };
 
@@ -89,7 +77,7 @@ export const getShortCode = async (request, response) => {
     if (!getCode) {
       return response.status(404).json({
         status: false,
-        error: "Not short code found",
+        error: "No short code found",
       });
     }
 
@@ -99,18 +87,14 @@ export const getShortCode = async (request, response) => {
       },
 
       {
-      $inc: { redirectCount: 1 },
+        $inc: { redirectCount: 1 },
+        lastSeenDate: new Date()
       },
       {
         new: true,
       }
     );
-
-    return response.status(302).json({
-      status: true,
-      location: code.url
-    });
-
+    return response.status(302).redirect(code.url);
   } catch (error) {
     return response.status(500).json({
       status: true,
@@ -125,10 +109,10 @@ export const getShortCodeStat = async (request, response) => {
 
     const getCode = await UrlShorten.findOne({ shortCode }).lean();
 
-   if (!getCode) {
+    if (!getCode) {
       return response.status(404).json({
         status: false,
-        error: "Not short code found",
+        error: "No short code found",
       });
     }
 
@@ -139,9 +123,8 @@ export const getShortCodeStat = async (request, response) => {
         startDate: getCode.startDate,
         lastSeenDate: getCode.lastSeenDate,
         redirectCount: getCode.redirectCount,
-      }
+      },
     });
-
   } catch (error) {
     return response.status(500).json({
       status: true,
